7eb20d726220159a0484da5ec5a13d3a40c1c500
codingma <454226880@qq.com>
2018-02-06 01:07:54 +0800

    add number 21

---
+++ number_0021_0040/Merge Two Sorted Lists.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        else:
+            if l1.val <= l2.val:
+                l1.next = self.mergeTwoLists(l1.next, l2)
+                return l1
+            else:
+                l2.next = self.mergeTwoLists(l1, l2.next)
+                return l2
